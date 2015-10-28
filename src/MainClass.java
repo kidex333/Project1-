780a9eb3ea65f007c43edaa4d95290bc226fb969
kpanicc <jcordobarom@gmail.com>
2015-10-28 12:01:08 +0100

    method1

--- src/MainClass.java
+++ src/MainClass.java
@@ -7,62 +7,130 @@ public class MainClass {
 		Scanner read = new Scanner(System.in);
 		System.out.print("How many rows are there in the plane? ");
 		numberOfRows = read.nextInt();
-		String[][] seats = new String[numberOfRows][4];
+		int lengthRows = (int) Math.log10(numberOfRows) + 1;
+		boolean[][] seats = new boolean[numberOfRows][4];
 		System.out.print("How much does the one-way ticket costs? ");
 		priceOfTicket = read.nextDouble();
-		byte action = 0;
+		int action = 0;
 		do
 		{
 			System.out.println("What do you want to do?");
 			System.out.println("1: Buy tickets");
 			System.out.println("2: Check");
 			System.out.println("3: Cancel tickets");
-			action = read.nextByte();
-			if (action > 3 || action < 1)
+			System.out.println("4: Exit the program");
+			action = read.nextInt();
+			if (action > 4 || action < 1)
 			{
 				System.out.println("The action selected is not valid");
 			}
 			else if (action == 1)
 			{
 				// true if one-way
-				int typeofticket = 0;
-				do
-				{
-					boolean typeOfTicket = true;
-					System.out.println("What type of ticket do you want to buy?");
-					System.out.println("1: One-Way ticket");
-					System.out.println("2: Return ticket");
-					typeofticket = read.nextInt();
-					
-					if (typeofticket == 1)
-					{
-						typeOfTicket = true;
-					}
-					else if (typeofticket == 2)
-					{
-						typeOfTicket = false;		
-					}
-					else
-					{
-						System.out.println("That type of ticket doesn't exists.");
-					}
-					}while (typeofticket > 2 || typeofticket < 1);
-					int numberOfTickets = 0;
-					do
-					{
-						System.out.print("How many tickets do you want? ");
-						numberOfTickets = read.nextInt();
-						if (numberOfTickets > 0 && numberOfTickets < 11)
-						{
-							System.out.print("Do you want an especific seat? ");
-						}
-						else
-						{
-							System.out.println("The number of tickets inserted is not valid.");
-						}
-					} while (numberOfTickets < 0 || numberOfTickets > 11);
+				
+				
 			}
-		}while (action > 3 || action < 1);
+			else if (action == 2)
+			{
+				
+			}
+			else if (action == 3)
+			{
+				
+			}
+			else
+			{
+				String confirmation;
+				System.out.print("Are you sure you want to exit the program? ");
+				confirmation = read.nextLine().toLowerCase();
+				if (confirmation.equals("yes") || confirmation.equals("y"))
+				{
+					System.exit(0);
+				}
+			}
+		}while (true);
 	}//main
+	/**
 	
+	*/
+	
+	static boolean[][] buyTicket(boolean[][] inSeats, double inPriceOfTicket)
+	{
+		boolean[][] seats = inSeats;
+		double price;
+		int tickets = 0;
+		do
+		{
+			System.out.print("How many tickets do you want to buy? ");
+			tickets = read.nextInt();
+			if (tickets < 0 || tickets > 10)
+				{
+					System.out.println("The number of tickets entered is not valid.");
+				}
+				else
+				{
+					boolean discount = tickets > 6 ? true : false;
+					int count = tickets;
+					while (count > 0)
+					{
+						String digits = "";
+						String letters = "";
+						boolean error = false;
+						int row = 0;
+						int column = 0;
+						do
+						{
+							digits = "";
+							letters = "";
+							System.out.println("Insert the number of the seat (1 - " + numberOfRows + ") (A - D)");
+							String seat = read.next().toUpperCase();
+							char[] foo = seat.toCharArray();								
+							for (char bar : foo)
+							{
+								if ((int) bar > 47 && (int) bar < 57)
+								{
+									digits += Character.toString(bar);
+									error = false;
+								}
+								else if ((int) bar > 64 && (int) bar < 68)
+								{
+									letters += Character.toString(bar);
+									error = false;
+								}
+								else
+								{
+									error = true;
+									break;
+								}									
+							}
+							if (letters.length() != 1 || Integer.parseInt(digits) > numberOfRows)
+							{
+								error = true;
+							}
+							
+							if (error)
+							{									
+								System.out.println("The seat entered is not valid.");
+							}
+							else
+							{
+								row = Integer.parseInt(digits) - 1;
+								column = (int) letters.charAt(0) - 65;
+								if (seats[row][column])
+								{										
+									System.out.println("That seat is busy.");
+								}									
+							}
+						} while(error || seats[row][column]);
+						
+						//do
+						//{
+						//	System
+						//}while
+						
+						count--;
+					}
+				}
+			}while (tickets < 0 || tickets > 10);
+	}//methodbuy
 }//class
